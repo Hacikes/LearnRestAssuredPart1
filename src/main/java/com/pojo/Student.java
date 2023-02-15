10bacb573835e8f74cf9a15337e58a3544d9e0d5
Hacikes <volna4753@gmail.com>
2023-02-15 16:13:40 +0300

    Добавлен lombok в pom.xml, с помощью него легче создавать геттеры, сеттеры и т д

--- src/main/java/com/pojo/Student.java
+++ src/main/java/com/pojo/Student.java
@@ -1,4 +1,20 @@
 package com.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+// Нажать на Refactor(в шапке) --> Delombok --> Setter/Getter
+// Генерирует Геттер и Сеттер, если введены @Getter, @Setter
+
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Student {
+    // генерирует геттер и сеттер и ими можно пользоваться в других классах
+
+    private  String name;
+    private String Text;
+
 }
