25340b073872620bb21ccce47575ed8ff966e1aa
Hacikes <volna4753@gmail.com>
2023-02-15 11:18:20 +0300

    Вариант использования конструктора и гетеров в классе Employee и Runner

--- src/main/java/com/pojo/Employee.java
+++ src/main/java/com/pojo/Employee.java
@@ -1,44 +1,28 @@
 package com.pojo;
 
 public class Employee {
-
+    // Приватная переменная используется, для того, чтобы можно было использовать сложную логику,
+    // например, для того, чтобы задать дефолтное значение, когда она нулевая. С переменными так не получится
     private int id;
-    // Геттер
+    private String Text;
+    private String email;
+    private String name;
+
+    // В данном случае сеттеры отсутствуют, так как мы устанавливаем значения конструктором.
+    // Конструктор в свою очередь создаёт неизменяемые объекты
     public int getId() {
         return id;
     }
-    // Сеттер
-    public void setId(int id) {
-        if(id != 0){
-            this.id = id;
-        }
-        else{
-            this.id = -1;
-        }
-        this.id = id;
+    public String getText() {
+        return Text;
     }
-
-    public String Text;
-    public String email;
-    // Приватная переменная используется, для того, чтобы можно было использовать сложную логику,
-    // например, для того, чтобы задать дефолтное значение, когда она нулевая. С переменными так не получится
-    private String name;
-
-    // Геттер для name
+    public String getEmail() {
+        return email;
+    }
     public String getName() {
         return name;
     }
-    // Сеттер для name
-    public Employee setName(String name) {
-        // if сделан, для случая, когда name будет нулевой
-        if(name != null){
-            this.name = name;
-        }
-        else{
-            this.name = "Ivan";
-        }
-        return this;
-    }
+
 
     //Конструктор
     public Employee(int id, String text, String email, String name) {
--- src/main/java/com/pojo/Runner.java
+++ src/main/java/com/pojo/Runner.java
@@ -3,15 +3,13 @@ package com.pojo;
 public class Runner {
 
 
-
     public static void main(String[] args){
         // Создаём из конструктора в Employee.java человека
+        // Конструктор создаёт неизменяемые объекты
         Employee employee = new Employee(14, "British", "Josh", "Josh1972@gmail.com");
         System.out.println(employee.getId());
         System.out.println(employee.getName());
-        // сл
-        employee.setName(null);//name="Josh Kern";
-        System.out.println(employee.getName());
+
 
 
 
