b307ca9842cd67bd93a0f387d40cff88c9f4d50b
Hacikes <volna4753@gmail.com>
2023-02-15 10:37:33 +0300

    Создание класса Employee и создание публичного класса с коструктором и использование его в классе Runner

---
+++ src/main/java/com/pojo/Employee.java
@@ -0,0 +1,17 @@
+package com.pojo;
+
+public class Employee {
+
+        public int id;
+        public String Text;
+        public String email;
+        public String name;
+
+        //Конструктор
+        public Employee(int id, String text, String email, String name) {
+            this.id = id;
+            Text = text;
+            this.email = email;
+            this.name = name;
+        }
+}
---
+++ src/test/java/com/tests/PostRequestUsingPojo.java
@@ -0,0 +1,17 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class PostRequestUsingPojo {
+
+    // Pojo - Plain Old Java Object
+    // {} --> Class file
+    // [] --> List<Type>
+    @Test
+    public void pojoTest(){
+
+
+
+
+    }
+}
