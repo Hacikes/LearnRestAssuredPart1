4cc62569f0cb95ebe060003ebccb774abd6d6e86
Hacikes <volna4753@gmail.com>
2023-02-15 11:08:35 +0300

    Добавление приватных переменных для примера в классе Employee, использование геттеров и сеттеров и из изменение всего этого в классе Runner

--- src/main/java/com/pojo/Employee.java
+++ src/main/java/com/pojo/Employee.java
@@ -2,16 +2,49 @@ package com.pojo;
 
 public class Employee {
 
-        public int id;
-        public String Text;
-        public String email;
-        public String name;
-
-        //Конструктор
-        public Employee(int id, String text, String email, String name) {
+    private int id;
+    // Геттер
+    public int getId() {
+        return id;
+    }
+    // Сеттер
+    public void setId(int id) {
+        if(id != 0){
             this.id = id;
-            Text = text;
-            this.email = email;
+        }
+        else{
+            this.id = -1;
+        }
+        this.id = id;
+    }
+
+    public String Text;
+    public String email;
+    // Приватная переменная используется, для того, чтобы можно было использовать сложную логику,
+    // например, для того, чтобы задать дефолтное значение, когда она нулевая. С переменными так не получится
+    private String name;
+
+    // Геттер для name
+    public String getName() {
+        return name;
+    }
+    // Сеттер для name
+    public Employee setName(String name) {
+        // if сделан, для случая, когда name будет нулевой
+        if(name != null){
             this.name = name;
         }
+        else{
+            this.name = "Ivan";
+        }
+        return this;
+    }
+
+    //Конструктор
+    public Employee(int id, String text, String email, String name) {
+        this.id = id;
+        Text = text;
+        this.email = email;
+        this.name = name;
+    }
 }
--- src/main/java/com/pojo/Runner.java
+++ src/main/java/com/pojo/Runner.java
@@ -7,11 +7,11 @@ public class Runner {
     public static void main(String[] args){
         // Создаём из конструктора в Employee.java человека
         Employee employee = new Employee(14, "British", "Josh", "Josh1972@gmail.com");
-        System.out.println(employee.id);
-        System.out.println(employee.name);
-        // Изменяем имя
-        employee.name="Josh Kern";
-        System.out.println(employee.name);
+        System.out.println(employee.getId());
+        System.out.println(employee.getName());
+        // сл
+        employee.setName(null);//name="Josh Kern";
+        System.out.println(employee.getName());
 
 
 
